5ca406c37ea59103f68ce74a00b0efe17c12299a
Ubuntu <ubuntu@ip-172-31-14-224.eu-west-1.compute.internal>
2018-09-21 11:23:13 +0000

    Added saved model weights

---
+++ HA1_load.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+import keras
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+
+
+train_datagen = keras.preprocessing.image.ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        'train',
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+        'val',
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary')
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(150, 150, 3)))
+model.add(Activation('elu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('elu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('elu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Dense(64))
+model.add(Activation('elu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+model.load_weights('first_try.h5')
+
+hist = model.fit_generator(
+        train_generator,
+        steps_per_epoch=2000,
+        epochs=5,
+        validation_data=validation_generator,
+        validation_steps=800)
+
+model.save_weights('second_try.h5')
+model.keras_model.save('saved_model.h5')
